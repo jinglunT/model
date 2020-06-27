cbfcb3646fbf813778317ff9307df20db4b870dc
Bestian Tang <bestian@gmail.com>
2020-06-27 09:44:51 +0800

    Update MyWork.vue

--- src/components/MyWork.vue
+++ src/components/MyWork.vue
@@ -42,6 +42,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+p {
+  display: inline-block;
+  max-width: 600px;
+  text-align: left;
+}
+
 img {
   width: 45%;
 }
