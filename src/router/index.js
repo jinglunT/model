5aa2c489bcc790f9db8510a0b0f859aaee6d752e
Bestian Tang <bestian@gmail.com>
2020-08-01 09:34:20 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Tools from '@/components/Tools'
 import MyWork from '@/components/MyWork'
+import MyWork1 from '@/components/MyWork1'
 import MyWork2 from '@/components/MyWork2'
 import Teach from '@/components/Teach'
 
@@ -24,6 +25,11 @@ export default new Router({
       path: '/mywork',
       name: 'MyWork',
       component: MyWork
+    },
+    {
+      path: '/mywork1',
+      name: 'MyWork1',
+      component: MyWork1
     },
     {
       path: '/mywork2',
